62b0bc49d0618b8f9e77363c17b7090c3c09f9c4
rasmus <rasmustirsgaard@gmail.com>
2020-07-12 00:14:51 +0200

    Updated DDQN_space

--- RL/DDQN_SPACE.py
+++ RL/DDQN_SPACE.py
@@ -17,18 +17,23 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.autograd as autograd 
 import torch.nn.functional as F
-import torchvision.transforms
+from torchvision import transforms
 import numpy as np
 from collections import deque
 import gym
+from time import time
+import glob
+import pickle
 from gym import spaces
 import cv2
 cv2.ocl.setUseOpenCL(False)
+from stable_baselines.common.atari_wrappers import EpisodicLifeEnv, FireResetEnv
+
 
 from IPython.display import clear_output
 import matplotlib.pyplot as plt
-from helper_functions import Replay_buffer, phi_transformer
-from DQN_model import CnnDQN_VAE
+from helper_functions_new import Replay_buffer
+from DQN_model import CnnDDQN_VAE
 USE_CUDA = torch.cuda.is_available()
 if USE_CUDA:
     torch.backends.cudnn.benchmark=True
@@ -58,14 +63,12 @@ def compute_td_loss(batch_size, iter_number):
     q_pred = Q_train(S)
     q_pred = q_pred.gather(1, a.unsqueeze(1)).squeeze(1)#q_val[range(batch_size),a]
     
-    writer.add_histogram('Predicted Q-values', q_pred, iter_number)
-    writer.add_histogram('Target Q-values', q_val_target, iter_number)
-    loss = (q_pred - Variable(q_val_target.data)).pow(2).mean()
-        
+    loss_func = nn.SmoothL1Loss()
+    loss = loss_func(q_pred, q_val_target)
     optimizer.zero_grad()
     loss.backward()
-    for param in Q_train.parameters():
-                param.grad.data.clamp_(-1, 1)
+    #for param in Q_train.parameters():
+    #            param.grad.data.clamp_(-1, 1)
     optimizer.step()
     
     return loss
@@ -99,6 +102,156 @@ class MaxAndSkipEnv(gym.Wrapper):
 
     def reset(self, **kwargs):
         return self.env.reset(**kwargs)
+    
+class MaxEnv(gym.Wrapper):
+    def __init__(self, env, skip=4):
+        """Return only every `skip`-th frame"""
+        gym.Wrapper.__init__(self, env)
+        self._skip = skip
+    def step(self, action):
+        """Repeat action, sum reward, and max over last observations."""
+        self.obs, reward, done, info = self.env.step(action)
+        self.old_obs = self.obs # we store old obs to max_pool
+        
+        # Max pool frames
+        obs = np.maximum(self.obs, self.old_obs)
+        return obs, reward, done, info
+
+    def reset(self, **kwargs):
+        self.old_frame = self.env.reset(**kwargs)
+        return self.old_frame
+    
+class SkipEnv(gym.Wrapper):
+    def __init__(self, env, skip=4):
+        """Return only every `skip`-th frame"""
+        gym.Wrapper.__init__(self, env)
+        self._skip = skip
+    def step(self, action):
+        """Repeat action, sum reward, and max over last observations."""
+        total_reward = 0.0
+        for i in range(self._skip):
+            obs, reward, done, info = self.env.step(action)
+            total_reward += reward
+            if done:
+                break
+        return obs, total_reward, done, info
+
+    def reset(self, **kwargs):
+        obs = self.env.reset(**kwargs)
+        return obs
+    
+class WarpFrame(gym.ObservationWrapper):
+    def __init__(self, env):
+        """
+        Warp frames to 84x84 as done in the Nature paper and later work.
+        :param env: (Gym Environment) the environment
+        """
+        gym.ObservationWrapper.__init__(self, env)
+        self.transform = transforms.Compose([
+            transforms.ToPILImage(),
+            transforms.Resize((128, 128)),
+            transforms.ToTensor(),
+        ])
+
+    def observation(self, frame):
+        """
+        returns the current observation from a frame
+        :param frame: ([int] or [float]) environment frame
+        :return: ([int] or [float]) the observation
+        """
+        return self.transform(frame)
+
+class FrameStack(gym.Wrapper):
+    def __init__(self, env, n_frames):
+        """Stack n_frames last frames.
+        Returns lazy array, which is much more memory efficient.
+        See Also
+        --------
+        stable_baselines.common.atari_wrappers.LazyFrames
+        :param env: (Gym Environment) the environment
+        :param n_frames: (int) the number of frames to stack
+        """
+        gym.Wrapper.__init__(self, env)
+        self.n_frames = n_frames
+        self.frames = deque([], maxlen=n_frames)
+        shp = env.observation_space.shape
+        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0], shp[1], shp[2] * n_frames),
+                                            dtype=env.observation_space.dtype)
+
+    def reset(self, **kwargs):
+        obs = self.env.reset(**kwargs)
+        for _ in range(self.n_frames):
+            self.frames.append(obs)
+        return self._get_ob()
+
+    def step(self, action):
+        obs, reward, done, info = self.env.step(action)
+        self.frames.append(obs)
+        return self._get_ob(), reward, done, info
+
+    def _get_ob(self):
+        assert len(self.frames) == self.n_frames
+        return list(self.frames)
+    
+    
+def wrap_deepmind(env, episode_life=True, clip_rewards=True, frame_stack=False, scale=False):
+    """
+    Configure environment for DeepMind-style Atari.
+    :param env: (Gym Environment) the atari environment
+    :param episode_life: (bool) wrap the episode life wrapper
+    :param clip_rewards: (bool) wrap the reward clipping wrapper
+    :param frame_stack: (bool) wrap the frame stacking wrapper
+    :param scale: (bool) wrap the scaling observation wrapper
+    :return: (Gym Environment) the wrapped atari environment
+    """
+    if episode_life:
+        env = EpisodicLifeEnv(env)
+    if 'FIRE' in env.unwrapped.get_action_meanings():
+        env = FireResetEnv(env)
+    env = WarpFrame(env)
+    if scale:
+        env = ScaledFloatFrame(env)
+    if clip_rewards:
+        env = ClipRewardEnv(env)
+    if frame_stack:
+        env = FrameStack(env, 4)
+    return env
+
+class Normalizer():
+    def __init__(self, num_inputs):
+        self.n = torch.zeros(num_inputs)
+        self.mean = torch.zeros(num_inputs)
+        self.mean_diff = torch.zeros(num_inputs)
+        self.var = torch.zeros(num_inputs)
+
+    def collect_and_norm(self, x):
+        ### Assumes input format is (1,C,H,W)
+        # Update values
+        for i in range(x.shape[1]):
+            self.n += 1
+            last_mean = self.mean.clone()
+            self.mean += (x[0,i]-self.mean)/self.n
+            self.mean_diff += (x[0,i]-last_mean)*(x[0,i]-self.mean)
+            self.var = self.mean_diff/self.n#torch.clamp(self.mean_diff/self.n, min=1e-2)
+        
+        # Return normalized image
+        obs_std = torch.sqrt(self.var)
+        return (x - self.mean)/obs_std
+
+    def normalize(self, inputs):
+        ### Assumes input format is (B,C,H,W)
+        obs_std = torch.sqrt(self.var)
+        return (inputs - self.mean)/obs_std
+    
+def proc_S(S):
+    # Convert from torch dim=(1,C,H,W) to numpy = (H,W,C)
+    S = S.squeeze(0)
+    S = S.permute(1,2,0)
+    S = S.numpy()
+    return S
+
+epsilon_by_frame = lambda n_frames: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * n_frames / epsilon_decay)
+
 
 ## Import SPACE
 import os
@@ -110,14 +263,24 @@ from utils import Checkpointer, MetricLogger
 from solver import get_optimizers
 import os.path as osp
 
+cwd = os.getcwd()
+# Change working directory for easier integration of SPACE
+os.chdir('/home/rasmus/DTU/VAE_RL/SPACE/src')
+sys.path.append('/home/rasmus/DTU/VAE_RL/SPACE/src')
+print(os.getcwd())
 cfg, task = get_config()
+cfg.checkpointdir = '/home/rasmus/DTU/VAE_RL/SPACE/output/checkpoints'
+cfg.resume_ckpt = '/home/rasmus/DTU/VAE_RL/github/VAE_RL/RL/VAE_models/model_000061001.pth'
 cfg.model = 'SPACE_atari'
 model = get_model(cfg)
 optimizer_fg, optimizer_bg = get_optimizers(cfg, model)
+SPACE_model_location = 'VAE_models/'# model save
+
 checkpointer = Checkpointer(osp.join(cfg.checkpointdir, cfg.exp_name), max_num=cfg.train.max_ckpt)
 checkpoint = checkpointer.load_last(cfg.resume_ckpt, model, optimizer_fg, optimizer_bg)
 model.cuda()
 model.eval()
+os.chdir(cwd) # Go back to correct working directory
 
 
 class SPACE_encoder:
@@ -126,9 +289,9 @@ class SPACE_encoder:
         
     def encode(self,x):
         with torch.no_grad():
-            x = torch.from_numpy(x.squeeze()).float().cuda()
+            x = torch.stack(x).cuda()
             # Normalize x
-            x = x/255
+            #x = x/255
             
             z = self.SPACE_model.forward(x, 10000)
             # Returns space of (n_phi, H*W, 42)
@@ -142,36 +305,46 @@ if not os.path.exists('models'):
     os.makedirs('models')
 
 #env = gym.make("PongNoFrameskip-v4")
-epsilon_by_frame = lambda n_frames: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * n_frames / epsilon_decay)
-
-env_id = "Riverraid-v0"
+env_id = "MontezumaRevenge-v0"
+#env_id = "Riverraid-v0"
 env = gym.make(env_id)
-env = MaxAndSkipEnv(env, skip=4)
+env = MaxEnv(env)
+env = wrap_deepmind(env, clip_rewards=False, frame_stack=True, episode_life=True)# We clip rewards in loss calculation
+env = SkipEnv(env) 
+
 n_phi = 4 # number of frames to stack
-im_dim = (256, 42)
-Q_train = CnnDQN_VAE((n_phi,) + im_dim, env.action_space.n, max_pool=True)
-Q_target = CnnDQN_VAE((n_phi,) + im_dim, env.action_space.n, max_pool=True)
+im_dim = (256, 41)
+Q_train = CnnDDQN_VAE((n_phi,) + im_dim, env.action_space.n, max_pool=True)
+Q_target = CnnDDQN_VAE((n_phi,) + im_dim, 
+                       env.action_space.n, 
+                       max_pool=True, 
+                       prov_bias = Q_train.return_bias())
 Q_target.load_state_dict(Q_train.state_dict())
 
 if USE_CUDA:
     Q_train = Q_train.cuda()
     Q_target = Q_target.cuda()
-#optimizer = optim.Adam(Q_train.parameters(), lr=0.0015)
-learning_rate = 0.000025
-optimizer = optim.RMSprop(Q_train.parameters(), lr=learning_rate, alpha=0.95,momentum = 0.95)
+
+lr_rate = 0.0000625
+optimizer = optim.Adam(Q_train.parameters(), lr=lr_rate)
 ## Saving parameters
-save_path = "models/DQN_"+env_id
+save_path = "models/DDQN_SPACE_"+env_id
+back_up_path = "backup/DDQN_SPACE_"+env_id+"/"
 save_freq = 10**5
-tensorboard_update_freq = 10
+tensorboard_update_freq = 100
+resume = True
+run_time = 100 # in seconds
 
 ## Runtime hyperparamters
-replay_initial = 50000
-replay_buffer_size = 100000
-replay_buffer = Replay_buffer(replay_buffer_size, im_dim, n_phi, S_dtype="float")
+notes = "Final run MontezumaRevenge-v0" # If anything should be noted in tensorboard
+
+## Runtime hyperparamters
+replay_initial = 100#50000
+replay_buffer_size = 10**5
 VAE_encoder = SPACE_encoder(model)
 
 epsilon_start = 1.0
-epsilon_final = 0.1
+epsilon_final = 0.01
 epsilon_decay = 10**6
 
 final_frame = 10**9
@@ -179,31 +352,65 @@ batch_size = 32
 gamma      = 0.99
 ## Update parameters
 update_freq = 4*n_phi
-target_update_freq = 30000
+target_update_freq = 3*10**4 # following Double Q-learing
+normalizer = Normalizer((256, 41))
 
-## Add hyper parameters to tensorboard
-writer.add_text("Environment", str(env_id))
-writer.add_text("Buffer size", str(replay_buffer_size))
-writer.add_text("Batch size", str(batch_size))
-writer.add_text("Learning rate", str(learning_rate))
-writer.add_text("Frames skip", str(4))
-writer.add_text("Type", "SPACE")
 
 episode_reward = 0
 episode_index = 0
 par_updates = 0
 n_frames = 0
+loss_list = []
+fps_list = []
+episode_reward = 0
+start_time = time()
+
+if resume:
+    ## Loading newest tensorboard
+    list_of_files = glob.glob('runs/*')
+    latest_file = max(list_of_files, key=os.path.getctime)
+    writer = SummaryWriter(latest_file)
+    
+    ## Load ANN models
+    # find newest backup
+    Q_target.load_state_dict(torch.load(back_up_path+"Q_target"))
+    Q_train.load_state_dict(torch.load(back_up_path+"Q_train"))
+    
+    replay_buffer = Replay_buffer(replay_buffer_size, im_dim, n_phi, save_type="disk", S_dtype="float16", cache=False, resume=True, save_location = back_up_path)
+    # Load last time step
+    with open(back_up_path+'objs.pkl', 'rb') as f:
+        n_frames, episode_index = pickle.load(f)
+    
+    
+else:
+    writer = SummaryWriter()
+    ## Add hyper parameters to tensorboard
+    writer.add_text("Environment", str(env_id))
+    writer.add_text("Buffer size", str(replay_buffer_size))
+    writer.add_text("Batch size", str(batch_size))
+    writer.add_text("Learning rate", str(lr_rate))
+    writer.add_text("Frames skip", str(4))
+    writer.add_text("Type", "SPACE")
+    writer.add_text("Notes", notes)
+    
+    replay_buffer = Replay_buffer(replay_buffer_size, im_dim, n_phi, save_type="disk", S_dtype="float16", cache=False, save_location = back_up_path)
+
+
+
+t1 = time()
 while n_frames<final_frame:
     done = False
     episode_index += 1
-    S = np.zeros((n_phi,) + (210,160,3), dtype="uint8")
-    S_obs = np.zeros((n_phi,) + (210,160,3), dtype="uint8")
-    S[n_phi-1] = env.reset()
-    S = phi_transformer(S, n_phi, im_size=[128,128], n_channels = 3)
-    S = VAE_encoder.encode(S)
-    episode_reward = 0
+    S = env.reset()
+    with torch.no_grad():
+        S = VAE_encoder.encode(S)
+        S = normalizer.collect_and_norm(S) # normalize
     
     while not done:
+        # Get fps
+        t2 = time()
+        fps_list.append(1/(t2-t1))
+        t1 = t2
         # Get random chance
         epsilon = epsilon_by_frame(n_frames)
         # Select action
@@ -214,38 +421,48 @@ while n_frames<final_frame:
                 a = Q_train.act(S)
 
         # Advance state
-        r = 0
-        for j in range(n_phi):
-            n_frames += 1
-            S_obs[j], r_temp, done, info = env.step(a)
-            r += r_temp
-            episode_reward += r_temp
-
-            if (done): # Check if game done
-                # Stich missing frames together
-                S_obs[(n_phi-j):n_phi] = S_obs[0:j]
-                S_obs[0:(n_phi-j)] = S_obs_prev[j:n_phi]
-                break
+        S_next, r, done, info = env.step(a)
+        with torch.no_grad():
+            S_next = VAE_encoder.encode(S_next)
+            S_next = normalizer.collect_and_norm(S_next) # normalize
+            replay_buffer.add_replay([proc_S(S), a, r, proc_S(S_next), done])
             
-        S_obs_prev = S_obs.copy()
-        S_next = phi_transformer(S_obs, n_phi, im_size=[128,128], n_channels = 3) # Transform input
-        S_next = VAE_encoder.encode(S_next)
-        n_frames += 1
-        replay_buffer.add_replay([S, a, r, S_next, done])
-
         S = S_next
+        episode_reward += r
+        n_frames += 1
 
         if (n_frames > replay_initial) & (n_frames % update_freq == 0):
-            par_updates += 1
+            # Update model
             loss = compute_td_loss(batch_size,n_frames)
-            if (par_updates % tensorboard_update_freq == 0):
-                writer.add_scalar('loss', loss.item(), n_frames)
+            loss_list.append(loss.item())
+            if (n_frames % tensorboard_update_freq == 0):
+                writer.add_scalar('loss', np.mean(loss_list), n_frames)
+                loss_list = []
 
-        if (par_updates % target_update_freq == 0):
-                    Q_target.load_state_dict(Q_train.state_dict())
+        if (n_frames % target_update_freq == 0): # This would instead be based on parameter updates following the nature paper, but in their code it is based on frames
+            # Update target network
+                Q_target.load_state_dict(Q_train.state_dict())
                 
         if (n_frames % save_freq == 0):
+            # Save model
             torch.save(Q_train, save_path)
     # Episode finished
-    print(episode_reward)
-    writer.add_scalar('Episode reward', episode_reward, episode_index)
+    if env.was_real_done:
+        print(episode_reward)
+        writer.add_scalar('Episode reward', episode_reward, episode_index)
+        writer.add_scalar('FPS', np.array(fps_list).mean(), episode_index)
+        fps_list = []
+        episode_reward = 0
+        
+        # Check if it is time stop
+        total_time = time()-start_time
+        if (total_time>=run_time):
+            # Save everything
+            torch.save(Q_target.state_dict(), back_up_path + "Q_target")
+            torch.save(Q_train.state_dict(), back_up_path + "Q_train")
+            replay_buffer.save_buffer()
+            with open(back_up_path+'objs.pkl', 'wb') as f:
+                pickle.dump([n_frames, episode_index], f)
+            # Stop program
+            print("Program stopped")
+            break
--- RL/DQN_model.py
+++ RL/DQN_model.py
@@ -150,3 +150,65 @@ class CnnDQN_VAE(nn.Module):
         q_value = self.forward(S)
         a  = q_value.max(1)[1].data[0]
         return a
+    
+    
+class CnnDDQN_VAE(nn.Module):
+    def __init__(self, input_shape, num_actions, max_pool=False, prov_bias = None):
+        super(CnnDDQN_VAE, self).__init__()
+        
+        self.input_shape = input_shape
+        self.num_actions = num_actions
+        
+        if max_pool: # Use maxpool in case input size to large
+            self.features = nn.Sequential(
+                nn.Conv2d(input_shape[0], 64, kernel_size=3, stride=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1),
+                nn.ReLU(),
+                nn.MaxPool2d(2)
+            )
+        else:
+            self.features = nn.Sequential(
+                nn.Conv2d(input_shape[0], 64, kernel_size=3, stride=1, padding=1),
+                nn.ReLU(),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1,padding=1),
+                nn.ReLU(),
+                nn.Conv2d(64, 64, kernel_size=3, stride=1,padding=1),
+                nn.ReLU()
+            )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(self.feature_size(), 512),
+            nn.ReLU(),
+            nn.Linear(512, self.num_actions,  bias=False)
+        )
+        if prov_bias == None:
+            self.bias = nn.Parameter(torch.zeros(num_actions)) # This is the shared bias layer
+        else: 
+            self.bias = prov_bias
+    
+    def feature_size(self):
+        return self.features(autograd.Variable(torch.zeros(1, *self.input_shape))).view(1, -1).size(1)
+    
+    def return_bias(self):
+        return self.bias
+    
+    def act(self, S):
+        S   = Variable(torch.FloatTensor(np.float32(S)))#.unsqueeze(0))
+        q_value = self.forward(S)
+        a  = q_value.max(1)[1].data[0]
+        return a
+    
+    def forward(self, x, bias = None):
+        # input format is (B,H,W,C)
+        # We assume data is normalized
+        #    x = x.permute(0,3,1,2)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+        x = x + self.bias
+        return x
